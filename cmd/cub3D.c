5ff2d217feababdb4e2faf1e2396fe0ba57c0800
Sevde Gul Emil <seemil@k1m09s11.42istanbul.com.tr>
2024-11-09 16:24:51 +0300

    mlx init

--- cmd/cub3D.c
+++ cmd/cub3D.c
@@ -60,5 +60,19 @@ int main(int ac, char **av)
 		perr(__func__, "validate_map failed");
 		return (dispose(game), 1);
 	}
+	
+	if (init_mlx(game) != OK)
+	{
+		perr(__func__, "mlx init failed");
+		return (dispose(game), 1);
+	}
+	mlx_hook(game->win_ptr, 17, 0, close_window, game); //çarpı işaretinden kapanacak
+    mlx_key_hook(game->win_ptr, esc_press, game); //esc ile kapancak 
+	if (image_up(game) != OK)
+	{
+		perr(__func__, "image up failed");
+		return (dispose(game), 1);
+	}
+	mlx_loop(game->mlx);
 	return (dispose(game), 0);
 }
---
+++ src/image_up.c
@@ -0,0 +1,16 @@
+#include "cub3D.h"
+
+t_err image_up(t_game *game)
+{
+    int w;
+    int h;
+
+    game->image.east = mlx_xpm_file_to_image(game->mlx, "textures/east.xpm", &w, &h);
+    game->image.north = mlx_xpm_file_to_image(game->mlx, "textures/north.xpm", &w, &h);
+    game->image.south = mlx_xpm_file_to_image(game->mlx, "textures/south.xpm", &w, &h);
+    game->image.west = mlx_xpm_file_to_image(game->mlx, "textures/west.xpm", &w, &h);
+
+    if (!game->image.east || !game->image.north || !game->image.south || !game->image.west)
+        return (perr(__func__, "xpm file to image failed(check xpm file path)"));
+    return (OK);
+}
